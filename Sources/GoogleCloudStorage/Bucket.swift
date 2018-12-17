d058c86e5a3d4afc1389d69debca15827038d555
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-17 11:01:38 -0600

    Documented Bucket service type

--- Sources/GoogleCloudStorage/Bucket.swift
+++ Sources/GoogleCloudStorage/Bucket.swift
@@ -1,8 +1,14 @@
 import Service
 
+/// A service used to store the name of the bucket to store files in.
 public struct Bucket: Service {
+    
+    /// The name of the bucket to use.
     public let name: String
     
+    /// Creates a new `Bucket` service instance.
+    ///
+    /// - Parameter name: The name of the bucket to use.
     public init(name: String) {
         self.name = name
     }
