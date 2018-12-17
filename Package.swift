05605d195d180d461f03d97153c45822480c9d59
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-17 06:41:45 -0600

    Installed google-cloud-provider package

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
         .library(name: "GoogleCloudStorage", targets: ["GoogleCloudStorage"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor-community/google-cloud-provider.git", from: "0.1.0"),
         .package(url: "https://github.com/skelpo/Storage.git", from: "0.1.0")
     ],
     targets: [
-        .target(name: "GoogleCloudStorage", dependencies: ["Storage"]),
+        .target(name: "GoogleCloudStorage", dependencies: ["Storage", "GoogleCloud"]),
         .testTarget(name: "GoogleCloudStorageTests", dependencies: ["GoogleCloudStorage"]),
     ]
 )
